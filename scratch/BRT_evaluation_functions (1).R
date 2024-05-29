1655e74d855801a2a6a4c2316573e2babd71e779
enazario11 <enazario@ucsc.edu>
2024-05-29 16:17:40 -0700

    set layout for test BRT explore script for new data

---
+++ scratch/BRT_evaluation_functions (1).R
@@ -0,0 +1,267 @@
+
+#' Script to calculate Area Under the receiver operator Curve (AUC) and True Skill Statistic (TSS) metrics for
+#' GAMM and BRT models
+#'
+#' Workflow:
+#' 0. Setup: load libraries, assign global objects and filepaths
+#' 1. Functions for calculating evaluation metrics for the following training/testing datasets:
+#'    1a. Full dataset
+#'    1b. k-fold
+#'    1c. Leave One Year Out
+
+
+# 0. ------------------- > Setup ####
+
+setwd("C:/Users/warto/Desktop/BRTs")
+
+#Load libraries
+{library(mlbench)
+library(dismo)
+library(gbm)
+library(lubridate)
+library(mgcv)
+library(qmap)
+library(dplyr)
+library(Hmisc)
+library(tidyverse)}
+
+all_data <- read.csv("FINAL_DATASET.csv")
+
+##SAMPLE OF THE FULL DATASET
+pres <- all_data[all_data$presabs==1,]
+abs <- all_data[all_data$presabs==0,]
+
+#absence_1 <- abs[sample(nrow(abs), nrow(pres), replace = FALSE), ]
+#presence_1 <- pres[sample(nrow(pres), 1000, replace = FALSE), ]
+#sample1 <- rbind(pres, absence_1)
+
+absence_2 <- abs[sample(nrow(abs), nrow(pres)*2, replace = FALSE), ]
+sample2 <- rbind(pres, absence_2)
+
+#test <- all_data[sample(nrow(all_data), 2000, replace = FALSE), ]
+
+pseudoR2.BRT <- function(x){
+  d2 <- 1-(x$self.statistics$mean.resid/x$self.statistics$mean.null)
+  return(d2)
+}
+
+#MODEL EVALUATION FOR PRES/ABS DATA (bernoulli)
+#Make function to 100/100  AUC test
+eval_100_percent <- function(dataInput, gbm.x, gbm.y, lr=lr, tc){
+  DataInput <- dataInput
+  Evaluations_100_percent <- as.data.frame(matrix(data=0,nrow=1,ncol=6))
+  colnames(Evaluations_100_percent) <- c("RMSE","Cor","C-index","PredRatio", "DevianceExplained","PseudoR2")
+  DataInput_train <- DataInput
+  DataInput_test <- DataInput
+  DataInput.kfolds <- gbm.step(data=DataInput_train, gbm.x= gbm.x, gbm.y = gbm.y, 
+                               family="bernoulli", tree.complexity=tc,
+                               learning.rate = lr, bag.fraction = 0.6,
+                               max.trees = 20000)
+  
+  show.gbm(DataInput.kfolds)
+  summary.gbm(DataInput.kfolds)
+  gbm.plot(DataInput.kfolds, write.title = FALSE)
+
+    preds <- predict.gbm(DataInput.kfolds, DataInput_test,
+                       n.trees=DataInput.kfolds$gbm.call$best.trees, type="response")
+
+    #preds <- predict.gbm(DataInput.kfolds, DataInput_test,
+    #                    n.trees=10000, type="response")
+    
+  #observed <- as_vector(DataInput_test %>% select(., gbm.y=gbm.y))
+  
+  observed <- as_vector(DataInput_test %>% pull(gbm.y))
+  
+  # if (min(observed) <= 0) {
+  #   preds <- exp(preds)
+  #   observed <- exp(observed)
+  # }
+  
+  ###D2 = (total deviance - cross validated residual deviance)/total deviance (Leathwick et al., 2006)
+  ext.residual.deviance <- calc.deviance(obs=observed, pred=preds, family="bernoulli", calc.mean=TRUE)
+  null.dev =  calc.deviance(observed,rep(mean(observed),length(observed)), family="bernoulli", calc.mean=T)
+  dev=(null.dev - ext.residual.deviance)/null.dev 
+  
+  Rsquared <- pseudoR2.BRT(DataInput.kfolds)
+  Evaluations_100_percent[1,1] <-sqrt(mean((preds-observed)^2))#RMSE
+  Evaluations_100_percent[1,2]<-cor(preds,observed, method="pearson") #Spearman correlation
+  Evaluations_100_percent[1,3]<-rcorr.cens(preds,observed)[1] #C-index
+  Evaluations_100_percent[1,4]<-sum(preds)/sum(observed) #Ratio of predictions to actual values
+  Evaluations_100_percent[1,5]<-dev #Ratio of predictions to actual values
+  Evaluations_100_percent[1,6]<-Rsquared #Ratio of predictions to actual values
+  
+  return(Evaluations_100_percent)
+  }
+
+##ALL ENVIRONMENTAL VARIABLES
+eval_100_sample2_01 <- eval_100_percent(dataInput=sample2,
+                                          gbm.x=c("adt_hist", "adt_sd_hist","bathy", "bathy_sd", "sla_hist","sla_sd_hist","sst_hist", "sst_sd_hist", "mld_hist", 
+                                             "mnkc_epi","zooc", "npp",  "l.chl_hist", 
+                                             "antic_amplitude", "antic_ndays", "antic_pos_norm", "antic_speed_rad", "antic_dist", "antic_rad_km",
+                                             "cic_amplitude", "cic_ndays", "cic_pos_norm", "cic_speed_rad", "cic_dist", "cic_rad_km",
+                                             "eke_hist", "FSLE_max", "Theta_max"),
+                                     gbm.y="presabs",
+                                     lr=0.01,
+                                     tc=3)
+
+
+
+#Number 2: 75/25% 
+eval_7525_modified <- function(dataInput, gbm.x, gbm.y, lr, tc=tc, family){
+  DataInput <- dataInput
+  Evaluations_7525 <- as.data.frame(matrix(data=0,nrow=1,ncol=6))
+  colnames(Evaluations_7525) <-  c("RMSE","Cor","C-index","PredRatio","DevianceExplained","PseudoR2")
+  DataInput_bound <- floor((nrow(DataInput)/4)*3)            #define % of training and test set
+  DataInput_train<- DataInput[sample(nrow(DataInput),DataInput_bound),]
+  DataInput_test<- DataInput[sample(nrow(DataInput),nrow(DataInput)-DataInput_bound),]
+  DataInput.kfolds <- gbm.step(data=DataInput_train, gbm.x= gbm.x, gbm.y = gbm.y, 
+                               family="bernoulli", tree.complexity=tc,
+                               learning.rate = lr, bag.fraction = 0.6)
+  show.gbm(DataInput.kfolds)
+  summary.gbm(DataInput.kfolds)
+  gbm.plot(DataInput.kfolds, write.title = FALSE)
+  
+   preds <- predict.gbm(DataInput.kfolds, DataInput_test,
+                       n.trees=DataInput.kfolds$gbm.call$best.trees, type="response")
+ 
+   #observed <- as_vector(DataInput_test %>% select(., gbm.y=gbm.y))
+  
+  observed <- as_vector(DataInput_test %>% pull(gbm.y))
+  
+  # if (min(observed) <= 0) {
+  #   preds <- exp(preds)
+  #   observed <- exp(observed)
+  # }
+  
+  ###D2 = (total deviance - cross validated residual deviance)/total deviance (Leathwick et al., 2006)
+  ext.residual.deviance <- calc.deviance(obs=observed, pred=preds, family="bernoulli", calc.mean=TRUE)
+  null.dev =  calc.deviance(observed,rep(mean(observed),length(observed)), family="bernoulli", calc.mean=T)
+  dev=(null.dev - ext.residual.deviance)/null.dev 
+  
+  Rsquared <- pseudoR2.BRT(DataInput.kfolds)
+  Evaluations_7525[1,1] <- sqrt(mean((preds-observed)^2))#RMSE
+  Evaluations_7525[1,2] <-cor(preds,observed, method="pearson") #Spearman correlation
+  Evaluations_7525[1,3]<-rcorr.cens(preds,observed)[1] #C-inde
+  Evaluations_7525[1,4]<-sum(preds)/sum(observed) #Ratio of predictions to actual values
+  Evaluations_7525[1,5]<-dev #External deviance explained
+  Evaluations_7525[1,6]<-Rsquared #Pseudo-Rsquared
+
+  return(Evaluations_7525)}
+
+##ALL ENVIRONMENTAL VARIABLES
+eval_7525_sample1 <- eval_7525_modified(dataInput=sample1,
+                                        gbm.x=c("adt_hist", "adt_sd_hist","bathy", "bathy_sd", "sla_hist","sla_sd_hist","sst_hist", "sst_sd_hist", "mld_hist", 
+                                             "mnkc_epi","zooc", "npp",  "l.chl_hist", 
+                                             "antic_amplitude", "antic_ndays", "antic_pos_norm", "antic_speed_rad",
+                                             "cic_amplitude", "cic_ndays", "cic_pos_norm", "cic_speed_rad",
+                                             "eke_hist", "FSLE_max", "Theta_max"),
+                                        gbm.y="presabs",
+                                        lr=0.005,
+                                        tc=3)
+
+
+
+#Number 3: k-folds split 
+
+kfolds_eval_brt_modified <- function(dataInput, gbm.x, gbm.y, lr, tc, bf){
+  DataInput <- dataInput
+  DataInput$Kset <- kfold(DataInput,10) #randomly allocate k groups
+  Evaluations_kfold <- as.data.frame(matrix(data=0,nrow=10,ncol=7))
+  colnames(Evaluations_kfold) <- c("k","RMSE","Cor","C-index","PredRatio","DevianceExplained","PseudoR2")
+  counter=1
+  for (k in 1:10){
+    print(k)
+    DataInput_train <- DataInput[DataInput$Kset!=k,]
+    DataInput_test <- DataInput[DataInput$Kset==k,]
+    DataInput.kfolds <- gbm.step(data=DataInput_train, gbm.x= gbm.x, gbm.y = gbm.y, 
+                                 family="bernoulli", tree.complexity=tc,
+                                 learning.rate = lr, bag.fraction = 0.6)
+
+    show.gbm(DataInput.kfolds)
+    summary.gbm(DataInput.kfolds)
+    gbm.plot(DataInput.kfolds, write.title = FALSE)
+    
+     preds <- predict.gbm(DataInput.kfolds, DataInput_test,
+                         n.trees=DataInput.kfolds$gbm.call$best.trees, type="response")
+    
+    observed <- as_vector(DataInput_test %>% pull(gbm.y))
+    
+    # if (min(observed) <= 0) {
+    #   preds <- exp(preds)
+    #   observed <- exp(observed)
+    # }
+    
+    ###D2 = (total deviance - cross validated residual deviance)/total deviance (Leathwick et al., 2006)
+    ext.residual.deviance <- calc.deviance(obs=observed, pred=preds, family="bernoulli", calc.mean=TRUE)
+    null.dev =  calc.deviance(observed,rep(mean(observed),length(observed)), family="bernoulli", calc.mean=T)
+    dev=(null.dev - ext.residual.deviance)/null.dev 
+    Rsquared <- pseudoR2.BRT(DataInput.kfolds)
+    
+    Evaluations_kfold[counter,1] <- k
+    Evaluations_kfold[counter,2] <- sqrt(mean((preds-DataInput_test$presabs)^2))#RMSE
+    Evaluations_kfold[counter,3] <-cor(preds,DataInput_test$presabs, method="pearson") #Spearman correlation
+    Evaluations_kfold[counter,4] <-rcorr.cens(preds,DataInput_test$presabs)[1] #C-index
+    Evaluations_kfold[counter,5] <-sum(preds)/sum(DataInput_test$presabs) #Ratio of predictions to actual values
+    Evaluations_kfold[counter,6]<-dev #External deviance explained
+    Evaluations_kfold[counter,7]<-Rsquared #Pseudo-R2 from BRT
+    counter=counter+1 
+  }
+  return(Evaluations_kfold)
+}
+##ALL ENVIRONMENTAL VARIABLES
+eval_kfolds_sample1 <- kfolds_eval_brt_modified(dataInput=sample1,
+                                              gbm.x=c("adt_hist", "adt_sd_hist","bathy", "bathy_sd", "sla_hist","sla_sd_hist","sst_hist", "sst_sd_hist", "mld_hist", 
+                                                      "mnkc_epi","zooc", "npp",  "l.chl_hist", 
+                                                      "antic_amplitude", "antic_ndays", "antic_pos_norm", "antic_speed_rad",
+                                                      "cic_amplitude", "cic_ndays", "cic_pos_norm", "cic_speed_rad",
+                                                      "eke_hist", "FSLE_max", "Theta_max"),
+                                              gbm.y="presabs",
+                                              lr=0.005,
+                                              tc=3)
+
+
+
+
+
+
+#Number 4: Leave one year out
+LOO_eval_modified <- function(dataInput, gbm.x, gbm.y, lr=lr, tc){
+  DataInput <- dataInput
+  Evaluations_LOO <- as.data.frame(matrix(data=0,nrow=1,ncol=7))
+  colnames(Evaluations_LOO) <-c("k","RMSE","Cor","C-index","PredRatio","DevianceExplained","PseudoR2")
+  counter=1
+  for (y in min(DataInput$Year):max(DataInput$Year)){
+    print(y)
+    DataInput_train <- DataInput[DataInput$Year!=y,]
+    DataInput_test <- DataInput[DataInput$Year==y,]
+    DataInput.loo <- gbm.step(data=DataInput_train, gbm.x= gbm.x, gbm.y = gbm.y, 
+                              family="bernoulli", tree.complexity=tc,
+                              learning.rate = lr, bag.fraction = 0.6)
+    preds <- predict.gbm(DataInput.loo, DataInput_test,
+                         n.trees=DataInput.loo$gbm.call$best.trees, type="response")
+    
+    observed <- as_vector(DataInput_test %>% pull(gbm.y))
+    
+    # if (min(observed) <= 0) {
+    #   preds <- exp(preds)
+    #   observed <- exp(observed)
+    # }
+    
+    ###D2 = (total deviance - cross validated residual deviance)/total deviance (Leathwick et al., 2006)
+    ext.residual.deviance <- calc.deviance(obs=observed, pred=preds, family="bernoulli", calc.mean=TRUE)
+    null.dev =  calc.deviance(observed,rep(mean(observed),length(observed)), family="bernoulli", calc.mean=T)
+    dev=(null.dev - ext.residual.deviance)/null.dev 
+    Rsquared <- pseudoR2.BRT(DataInput.kfolds)
+    
+    Evaluations_LOO[counter,1] <- y
+    Evaluations_LOO[counter,2] <- sqrt(mean((preds-observed)^2))#RMSE
+    Evaluations_LOO[counter,3] <-cor(preds,observed, method="pearson") #Spearman correlation
+    Evaluations_LOO[counter,4] <-rcorr.cens(preds,observed)[1] #C-index
+    Evaluations_LOO[counter,5] <-sum(preds)/sum(observed) #Ratio of predictions to actual values
+    Evaluations_LOO[counter,6] <- dev #Ratio of predictions to actual values
+    #Evaluations_LOO[counter,7] <- Rsquared #Ratio of predictions to actual values
+    counter=counter+1 
+    
+  }
+  return(Evaluations_LOO)}
+
--- scripts/6a_BRT_CMEM.R
+++ scripts/6a_BRT_CMEM.R
@@ -1,31 +1,31 @@
-
-#libraries
+### libraries ####
 library(tidyverse)
 library(gbm)
 library(dismo)
 library(here);here <- here::here
+library(ggBRT)
 
 set.seed(1004)
 
-#load data w/ covars
-dat <- readRDS(here("data/locs_w_covar/cmems/cmem_locs_covar_AGI_0m.rds"))
-dat$bathy <- replace(dat$bathy, dat$bathy == "NaN", NA)
-head(dat)
+### load data w/ covars ####
+dat0 <- readRDS(here("data/locs_w_covar/psat_spot/cmem_locs_covar_AGI_distcoast_0m.rds"))
+dat0$bathy <- replace(dat0$bathy, dat0$bathy == "NaN", NA)
+head(dat0)
 
 #originally, PA = 0 means a true position. Change so PA = 1 means a true position for fitting the BRT
-dat$PA <- replace(dat$PA, dat$PA == 1, 2) #change PAs to temporarily equal 2
-dat$PA <- replace(dat$PA, dat$PA == 0, 1) #change true positions to a 1
-dat$PA <- replace(dat$PA, dat$PA == 2, 0) #change PA positions to a 0
+dat0$PA <- replace(dat0$PA, dat0$PA == 1, 2) #change PAs to temporarily equal 2
+dat0$PA <- replace(dat0$PA, dat0$PA == 0, 1) #change true positions to a 1
+dat0$PA <- replace(dat0$PA, dat0$PA == 2, 0) #change PA positions to a 0
 
-dat_pos <- dat %>% filter(PA == 1)
-dat_pa <- dat %>% filter(PA == 0)
+dat_pos0 <- dat0 %>% filter(PA == 1)
+dat_pa0 <- dat0 %>% filter(PA == 0)
 
-#randomly select one PA for each tag
-dat2 <- NULL
-for(i in 1:length(unique(dat_pa$tag))){
+### randomly select one PA for each tag ####
+dat_temp <- NULL
+for(i in 1:length(unique(dat_pa0$tag))){
   #select current id
-  curr_ID <- unique(dat_pa$tag)[i]
-  temp_df <- dat_pa[dat_pa$tag %in% curr_ID,]
+  curr_ID <- unique(dat_pa0$tag)[i]
+  temp_df <- dat_pa[dat_pa0$tag %in% curr_ID,]
   
   #sample 52 id's randomly
   temp_rep_ID <- sample(unique(temp_df$rep), 1, replace = FALSE)
@@ -34,13 +34,13 @@ for(i in 1:length(unique(dat_pa$tag))){
   temp_df2 <- temp_df[temp_df$rep %in% temp_rep_ID, ]
   
   #combine in a single df
-  dat2 <- rbind(dat2, temp_df2)
+  dat_temp <- rbind(dat_temp, temp_df2)
   
 }
 
-dat2 <- rbind(dat2, dat_pos)
+dat_brt0 <- rbind(dat_temp, dat_pos)
 
-#glm and gam test 
+### glm and gam test ####
 m1 <- glm(PA ~ lat + thetao_mean + so_mean + mlotst_mean + lon + zos_mean + uo_mean + vo_mean + chl_mean + nppv_mean + o2_mean + bathy + bathy_sd + AGI_0m, data = dat)
 summary(m1)
 
@@ -48,30 +48,57 @@ m2 <- gam(PA ~ s(lat) + s(thetao_mean) + s(so_mean) + s(mlotst_mean) + s(lon) + 
 test_m <- gam(PA ~ s(AGI_0m) + s(thetao_mean), random=~(1|tag),family=binomial, data=na.omit(dat), bs = "cs", method = "REML")
 test <- plot(test_m, residuals = TRUE)
 
-#BRT starting point --- replace below once I select the model parameters from the explore doc
-dat_train <- dat2 %>% sample_frac(0.75)
-dat_test <- dat2 %>% sample_frac(0.25)
+### BRT starting point ####
+dat_train0 <- dat_brt0 %>% sample_frac(0.75)
+dat_test0 <- dat_brt0 %>% sample_frac(0.25)
 
-try(test <- dismo::gbm.step(data = dat_train, gbm.x = c(3:4, 9:15, 17, 18, 20), gbm.y = 5,
+try(test <- dismo::gbm.step(data = dat_train0, gbm.x = c(8:19, 23), gbm.y = 5,
                                               family = "bernoulli", tree.complexity = 5,
                                               learning.rate = 0.01, bag.fraction = 0.75, 
                             silent = TRUE, 
                             plot.main = TRUE))
 
-#explore outputs
-gbm.plot(test, nplots = 14, plot.layout = c(4,4), write.title = FALSE) #plots relative influence of each predictor across its range
-summary(test) #presents in a table and figure the relative influence
-gbm.plot.fits(test)
+#### explore outputs ####
+ggBRT::ggPerformance(test)
 
-#find and plot interactions 
-#ID
-test_int <- gbm.interactions(test)
-test_int$interactions
-test_int$rank.list
-#plot
-gbm.perspec(test, 13, 2, theta = 30) #change first two numbers to the pair of predictors to plot
+gbm.plot(test, nplots = 13, plot.layout = c(3,4), write.title = FALSE) 
+
+#relative influence of predictors
+ggBRT::ggInfluence(test)
+
+#find the 5 most important pairwise interactions 
+AGI_int <- gbm.interactions(test)
+AGI_int$rank.list
+
+#plot some interactions of interest
+gbm.perspec(test, 12, 1)
+gbm.perspec(test, 12, 2)
 
 #predictions
-preds <- predict.gbm(test, Anguilla_test,
-                     n.trees=angaus.tc5.lr005$gbm.call$best.trees, type="response")
-calc.deviance(obs=Anguilla_test$Angaus_obs, pred=preds, calc.mean=TRUE)
+preds <- predict.gbm(test, dat_test0, 
+                     n.trees = test$gbm.call$best.trees, 
+                     type = "response")
+
+calc.deviance(obs = dat_test0$PA, preds) #get % deviance
+
+dat_pred0 <- cbind(dat_test0$PA, preds)
+pres0 <- dat_pred0[dat_pred0[,1] == 1, 2]
+abs0 <- dat_pred0[dat_pred0[,1] == 0, 2]
+
+#evaluate (AUC, TSS, TPR)
+e = evaluate(p = pres0, a = abs0)
+plot(e, 'TPR')
+plot(e, 'TNR')
+plot(e, 'ROC')
+boxplot(e)
+density(e)
+mean(e@TPR) #TPR
+max(e@TPR + e@TNR -1) #TSS
+
+dev_eval_brt=function(model_object){ #provides % deviance for model selection -- refer to PA paper for other model eval functoins for dismo pacakge model object
+  null <- model_object$self.statistics$null
+  res <- model_object$self.statistics$resid
+  dev=((null - res)/null)*100 
+  return(dev)
+}
+
