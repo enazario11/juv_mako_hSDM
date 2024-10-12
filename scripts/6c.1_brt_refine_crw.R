c35d94978d56ba2bca03ffab67f0ca6251c82eae
enazario11 <enazario@ucsc.edu>
2024-10-12 11:36:42 -0700

    brt eval

--- scripts/6c.1_brt_refine_crw.R
+++ scripts/6c.1_brt_refine_crw.R
@@ -4,6 +4,8 @@ library(gbm)
 library(dismo)
 library(here);here <- here::here
 library(ggBRT)
+library(foreach)
+library(doParallel)
 
 set.seed(1004)
 
@@ -36,7 +38,7 @@ dat_base_s$pred_var <- pred_var
 #### Add seasonal and annual data to daily data df ####
 dat_do_all <- cbind(dat_do_d, dat_do_s$o2_mean_0m, dat_do_s$o2_mean_60m, dat_do_s$o2_mean_250m, dat_do_a$o2_mean_0m, dat_do_a$o2_mean_60m, dat_do_a$o2_mean_250m)
 dat_do_all <- dat_do_all %>%
-  rename("o2_mean_0m_seas" = "dat_do_s$o2_mean_0m", 
+  dplyr::rename("o2_mean_0m_seas" = "dat_do_s$o2_mean_0m", 
          "o2_mean_60m_seas" = "dat_do_s$o2_mean_60m", 
          "o2_mean_250m_seas" = "dat_do_s$o2_mean_250m", 
          "o2_mean_0m_ann" = "dat_do_a$o2_mean_0m", 
@@ -45,7 +47,7 @@ dat_do_all <- dat_do_all %>%
 
 dat_agi_all <- cbind(dat_agi_d, dat_agi_s$AGI_0m, dat_agi_s$AGI_60m, dat_agi_s$AGI_250m, dat_agi_a$AGI_0m, dat_agi_a$AGI_60m, dat_agi_a$AGI_250m)
 dat_agi_all <- dat_agi_all %>%
-  rename("AGI_0m_seas" = "dat_agi_s$AGI_0m",
+  dplyr::rename("AGI_0m_seas" = "dat_agi_s$AGI_0m",
          "AGI_60m_seas" = "dat_agi_s$AGI_60m", 
          "AGI_250m_seas" = "dat_agi_s$AGI_250m", 
          "AGI_0m_ann" = "dat_agi_a$AGI_0m", 
@@ -372,8 +374,76 @@ mean_prob <- rowMeans(pred_testdata)
 (perf_mean_prob <- mecofun::evalSDM(test_df$PA, mean_prob))
 
 ### Leave one year out evaluation ####
+#Subset one out (SOO) function originally developed by Heather and Nerea, modified by EN here
+SOO_eval <- function(df, gbm.x, gbm.y, lr = 0.05, tc = 3, family = "bernoulli"){
+  DataInput = df %>% mutate(Year=year(dt))
+  
+  Evaluations_LOO <- as.data.frame(matrix(data=0,nrow=length(unique(DataInput$Year)),ncol=10))
+  colnames(Evaluations_LOO) <- c("Year","Deviance","AUC","TSS","TPR","TNR","AveragePredAbs","AveragePredPres","N. test points","% left out")
+    
+  counter=1
+  for (y in min(DataInput$Year):max(DataInput$Year)) {
+    print(y)
+    
+    DataInput_train <- DataInput[DataInput$Year!=y,]
+    DataInput_test <- DataInput[DataInput$Year==y,]
+    
+    DataInput.loo <- gbm.step(data = DataInput_train, gbm.x = gbm.x, gbm.y = gbm.y, 
+                                family = family, tree.complexity = tc,
+                                learning.rate = lr, bag.fraction = 0.75)
+    
+    preds <- predict.gbm(DataInput.loo, DataInput_test,
+                         n.trees = DataInput.loo$gbm.call$best.trees, type="response")
+  
+    null <- DataInput.loo$self.statistics$null
+    res <- DataInput.loo$self.statistics$resid
+    dev=((null - res)/null)*100 
+    
+    d <- cbind(DataInput_test$PA, preds)
+    test = as.data.frame(d)
+    colnames(test) <- c("PA", "preds")
+    test= test %>% group_by(PA) %>% summarise(mean(preds))
+    
+    pres <- d[d[,1]==1,2]
+    abs <- d[d[,1]==0,2]
+    
+    if(length(pres)>0 & length(abs)>0){
+      e <- evaluate(p=pres, a=abs)
+      thresh=threshold(e)$equal_sens_spec
+      ind=which(abs(e@t-thresh)==min(abs(e@t-thresh)))
+      Evaluations_LOO[counter,1] <- y
+      Evaluations_LOO[counter,2] <- dev
+      Evaluations_LOO[counter,3] <- e@auc
+      Evaluations_LOO[counter,4] <- max(e@TPR + e@TNR-1)
+      Evaluations_LOO[counter,5] <- e@TPR[ind]
+      Evaluations_LOO[counter,6] <- e@TNR[ind]
+      Evaluations_LOO[counter,7] <- test[1,2]
+      Evaluations_LOO[counter,8] <- test[2,2]
+      Evaluations_LOO[counter,9] <- nrow(DataInput_test)
+      Evaluations_LOO[counter,10] <- nrow(DataInput_test)/nrow(DataInput)*100
+      counter=counter+1
+    }
+  }
+  return(Evaluations_LOO)}
 
+#### base model ####
+gbm_base = colnames(dat_base_d[, c(8:10, 15:18)])
+soo_year_base <- SOO_eval(df = dat_base_d, gbm.x = gbm_base, gbm.y = "PA")
+saveRDS(soo_year_base, here("data/brt/mod_outputs/crw/evaluation/soo_year_base.rds"))
 
+#### do model ####
+gbm_do = colnames(dat_do_all[, c(8:11, 16:19, 22, 24, 26, 27, 29)])
+soo_year_do <- SOO_eval(df = dat_do_all, gbm.x = gbm_do, gbm.y = "PA")
+saveRDS(soo_year_do, here("data/brt/mod_outputs/crw/evaluation/soo_year_do.rds"))
 
+#### agi model ####
+gbm_agi = colnames(dat_agi_all[, c(8:10, 15:19, 22, 24, 26, 27, 29)])
+soo_year_agi <- SOO_eval(df = dat_agi_all, gbm.x = gbm_agi, gbm.y = "PA")
+saveRDS(soo_year_agi, here("data/brt/mod_outputs/crw/evaluation/soo_year_agi.rds"))
+
+#### do,agi combo model ####
+gbm_do_agi = colnames(dat_do_agi_all[, c(8:11, 16:19, 24, 27, 31,33, 35)])
+soo_year_combo <- SOO_eval(df = dat_do_agi_all, gbm.x = gbm_do_agi, gbm.y = "PA")
+saveRDS(soo_year_combo, here("data/brt/mod_outputs/crw/evaluation/soo_year_combo.rds"))
 
 
