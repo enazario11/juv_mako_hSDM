7f10a486cb07a4c8f591a1ffa452dca755b29eec
enazario11 <enazario@ucsc.edu>
2024-12-02 16:01:07 -0800

    revisions

--- functions/avg_functions.R
+++ functions/avg_functions.R
@@ -595,11 +595,6 @@ hsi_maps_difference_enso_avg <- function(neut_rast_folder, enso_rast_folder, ens
   
   #creating map dfs -------------------------------------------------------------------------------------------------
   #predict
-  base_mod_files <- list.files(here("data/brt/mod_outputs/perf_metric_iters/base"), full.names = TRUE)
-  do_mod_files <- list.files(here("data/brt/mod_outputs/perf_metric_iters/do"), full.names = TRUE)
-  agi_mod_files <- list.files(here("data/brt/mod_outputs/perf_metric_iters/agi"), full.names = TRUE)
-  combo_mod_files <- list.files(here("data/brt/mod_outputs/perf_metric_iters/combo"), full.names = TRUE)
-  
   base_list <- rast()
   do_list <- rast()
   agi_list <- rast()
@@ -612,33 +607,38 @@ hsi_maps_difference_enso_avg <- function(neut_rast_folder, enso_rast_folder, ens
     
     #predict
     base_mod <- readRDS(base_mod_files[i])
-    base_pred <- predict(base_rast, base_mod, type = "response", n.tress = base_mod$gbm.call$best.trees, na.rm = FALSE)
+    base_pred <- predict(base_rast_enso, base_mod, type = "response", n.tress = base_mod$gbm.call$best.trees, na.rm = FALSE)
     base_pred <- crop(base_pred, extent)
     base_list <- c(base_list, base_pred)
     
     do_mod <- readRDS(do_mod_files[i])
-    do_pred <- predict(do_rast, do_mod, type = "response", n.tress = do_mod$gbm.call$best.trees, na.rm = FALSE)
+    do_pred <- predict(do_rast_enso, do_mod, type = "response", n.tress = do_mod$gbm.call$best.trees, na.rm = FALSE)
     do_pred <- crop(do_pred, extent)
     do_list <- c(do_list, do_pred)
     
     agi_mod <- readRDS(agi_mod_files[i])
-    agi_pred <- predict(agi_rast, agi_mod, type = "response", n.tress = agi_mod$gbm.call$best.trees, na.rm = FALSE)
+    agi_pred <- predict(agi_rast_enso, agi_mod, type = "response", n.tress = agi_mod$gbm.call$best.trees, na.rm = FALSE)
     agi_pred <- crop(agi_pred, extent)
     agi_list <- c(agi_list, agi_pred)
     
     do_agi_comb <- readRDS(combo_mod_files[i])
-    do_agi_combo <- predict(do_agi_rast, do_agi_comb, type = "response", n.trees = do_agi_comb$gbm.call$best.trees, na.rm = FALSE)
+    do_agi_combo <- predict(do_agi_rast_enso, do_agi_comb, type = "response", n.trees = do_agi_comb$gbm.call$best.trees, na.rm = FALSE)
     do_agi_combo <- crop(do_agi_combo, extent)
     combo_list <- c(combo_list, do_agi_combo)
     
   }
   
+  base_avg_enso <- mean(base_list)
+  do_avg_enso <- mean(do_list)
+  agi_avg_enso <- mean(agi_list)
+  combo_avg_enso <- mean(combo_list)
+  
   # create difference rasters ------------------------------------------------------------------------------------------
   #make difference maps
-  diff_base <- diff(c(base_pred, base_pred_enso))*100
-  diff_agi <- diff(c(agi_pred, agi_pred_enso))*100
-  diff_do <- diff(c(do_pred, do_pred_enso))*100
-  diff_do_agi_combo <- diff(c(do_agi_pred, do_agi_pred_enso))*100
+  diff_base <- diff(c(base_avg_neut, base_avg_enso))*100
+  diff_agi <- diff(c(agi_avg_neut, agi_avg_enso))*100
+  diff_do <- diff(c(do_avg_neut, do_avg_enso))*100
+  diff_do_agi_combo <- diff(c(combo_avg_neut, combo_avg_enso))*100
   
   #plot maps --------------------------------------------------------------------------------------------------------
   #land files
@@ -647,15 +647,15 @@ hsi_maps_difference_enso_avg <- function(neut_rast_folder, enso_rast_folder, ens
   
   #base map
   #calculate percent area polygon takes up of raster 
-  base_hsi <- raster::clamp(base_pred, lower = 0.75, values = FALSE) #create raster of values with HSI > 0.75
+  base_hsi <- raster::clamp(base_avg_neut, lower = 0.75, values = FALSE) #create raster of values with HSI > 0.75
   hsi_area_base <- expanse(base_hsi)
-  rast_area_base <- expanse(base_pred)
+  rast_area_base <- expanse(base_avg_neut)
   perc_area_base <- (hsi_area_base/rast_area_base$area[1])*100
   
   #enso area
-  base_hsi_enso <- raster::clamp(base_pred_enso, lower = 0.75, values = FALSE) #create raster of values with HSI > 0.75
+  base_hsi_enso <- raster::clamp(base_avg_enso, lower = 0.75, values = FALSE) #create raster of values with HSI > 0.75
   hsi_area_base_enso <- expanse(base_hsi_enso)
-  rast_area_base_enso <- expanse(base_pred_enso)
+  rast_area_base_enso <- expanse(base_avg_enso)
   perc_area_base_enso <- (hsi_area_base_enso/rast_area_base_enso$area[1])*100
   
   #calculate difference
@@ -684,13 +684,13 @@ hsi_maps_difference_enso_avg <- function(neut_rast_folder, enso_rast_folder, ens
   do_hsi <- raster::clamp(do_pred, lower = 0.75, values = FALSE) #create raster of values with HSI > 0.75
   
   hsi_area_do <- expanse(do_hsi)
-  rast_area_do <- expanse(do_pred)
+  rast_area_do <- expanse(do_avg_neut)
   perc_area_do <- (hsi_area_do/rast_area_do$area[1])*100
   
   #enso area
-  do_hsi_enso <- raster::clamp(do_pred_enso, lower = 0.75, values = FALSE) #create raster of values with HSI > 0.75
+  do_hsi_enso <- raster::clamp(do_avg_enso, lower = 0.75, values = FALSE) #create raster of values with HSI > 0.75
   hsi_area_do_enso <- expanse(do_hsi_enso)
-  rast_area_do_enso <- expanse(do_pred_enso)
+  rast_area_do_enso <- expanse(do_avg_enso)
   perc_area_do_enso <- (hsi_area_do_enso/rast_area_do_enso$area[1])*100
   
   #calculate difference
@@ -735,16 +735,16 @@ hsi_maps_difference_enso_avg <- function(neut_rast_folder, enso_rast_folder, ens
   
   #agi map
   #calculate percent area polygon takes up of raster 
-  agi_hsi <- raster::clamp(agi_pred, lower = 0.75, values = FALSE) #create raster of values with HSI > 0.75
+  agi_hsi <- raster::clamp(agi_avg_neut, lower = 0.75, values = FALSE) #create raster of values with HSI > 0.75
   
   hsi_area_agi <- expanse(agi_hsi)
-  rast_area_agi <- expanse(agi_pred)
+  rast_area_agi <- expanse(agi_avg_neut)
   perc_area_agi <- (hsi_area_agi/rast_area_agi$area[1])*100
   
   #enso area
-  agi_hsi_enso <- raster::clamp(agi_pred_enso, lower = 0.75, values = FALSE) #create raster of values with HSI > 0.75
+  agi_hsi_enso <- raster::clamp(agi_avg_enso, lower = 0.75, values = FALSE) #create raster of values with HSI > 0.75
   hsi_area_agi_enso <- expanse(agi_hsi_enso)
-  rast_area_agi_enso <- expanse(agi_pred_enso)
+  rast_area_agi_enso <- expanse(agi_avg_enso)
   perc_area_agi_enso <- (hsi_area_agi_enso/rast_area_agi_enso$area[1])*100
   
   #calculate difference
@@ -789,16 +789,16 @@ hsi_maps_difference_enso_avg <- function(neut_rast_folder, enso_rast_folder, ens
   
   #do agi combo map
   #calculate percent area polygon takes up of raster 
-  combo_hsi <- raster::clamp(do_agi_pred, lower = 0.75, values = FALSE) #create raster of values with HSI > 0.75
+  combo_hsi <- raster::clamp(combo_avg_neut, lower = 0.75, values = FALSE) #create raster of values with HSI > 0.75
   
   hsi_area_combo <- expanse(combo_hsi)
-  rast_area_combo <- expanse(do_agi_pred)
+  rast_area_combo <- expanse(combo_avg_neut)
   perc_area_combo <- (hsi_area_combo/rast_area_combo$area[1])*100
   
   #enso area
-  combo_hsi_enso <- raster::clamp(do_agi_pred_enso, lower = 0.75, values = FALSE) #create raster of values with HSI > 0.75
+  combo_hsi_enso <- raster::clamp(combo_avg_enso, lower = 0.75, values = FALSE) #create raster of values with HSI > 0.75
   hsi_area_combo_enso <- expanse(combo_hsi_enso)
-  rast_area_combo_enso <- expanse(do_agi_pred_enso)
+  rast_area_combo_enso <- expanse(combo_avg_enso)
   perc_area_combo_enso <- (hsi_area_combo_enso/rast_area_combo_enso$area[1])*100
   
   #calculate difference
@@ -847,6 +847,118 @@ hsi_maps_difference_enso_avg <- function(neut_rast_folder, enso_rast_folder, ens
   
   #end function  
 }
+
+
+# average predictor relative importance ####
+avg_pred <- function(base_mods, do_mods, agi_mods, combo_mods, iter = 20){
+  
+  #list models
+  base_mod_files <- base_mods
+  do_mod_files <- do_mods
+  agi_mod_files <- agi_mods
+  combo_mod_files <- combo_mods
+  
+  inf_df <- NULL
+  for(i in 1:iter){
+    
+  #base model 
+  base_inf <- as.data.frame(ggBRT::ggInfluence(readRDS(base_mod_files[i]), plot = FALSE)) %>% rownames_to_column()
+  colnames(base_inf) <- c("Predictor_variable", "relative_influence")
+  base_inf$Predictor_variable <- gsub("bathy_mean", "z", base_inf$Predictor_variable)
+  base_inf$Predictor_variable <- gsub("temp_mean", "temp", base_inf$Predictor_variable)
+  base_inf$Predictor_variable <- gsub("sal_mean", "sal", base_inf$Predictor_variable)
+  base_inf$Predictor_variable <- gsub("chl_mean", "chl-a", base_inf$Predictor_variable)
+  base_inf$Predictor_variable <- gsub("bathy_sd", "z_sd", base_inf$Predictor_variable)
+  base_inf$Predictor_variable <- gsub("ssh_mean", "SSH", base_inf$Predictor_variable)
+  base_inf$Predictor_variable <- gsub("mld_mean", "MLD", base_inf$Predictor_variable)
+  
+  base_df <- data.frame(model = c("Base", "Base", "Base", "Base", "Base", "Base", "Base"), 
+                       var = base_inf$Predictor_variable, 
+                       inf_val = base_inf$relative_influence)
+  
+  base_df <- base_df %>% mutate(iter = i)
+  
+  inf_df <- rbind(inf_df, base_df)
+  
+  }
+  
+  #do model
+  for(i in 1:iter){
+  
+  do_inf <- as.data.frame(ggBRT::ggInfluence(readRDS(do_mod_files[i]), plot = FALSE)) %>% rownames_to_column()
+  colnames(do_inf) <- c("var", "inf_val")
+  do_inf$var <- gsub("\\<o2_mean_0m\\>", "DO, daily, 0m", do_inf$var)
+  do_inf$var <- gsub("o2_mean_250m_ann", "DO, annual, 250m", do_inf$var)
+  do_inf$var <- gsub("o2_mean_0m_seas", "DO, seasonal, 0m", do_inf$var)
+  do_inf$var <- gsub("o2_mean_250m_seas", "DO, seasonal, 250m", do_inf$var)
+  do_inf$var <- gsub("temp_mean", "temp", do_inf$var)
+  do_inf$var <- gsub("sal_mean", "sal", do_inf$var)
+  do_inf$var <- gsub("bathy_mean", "z", do_inf$var)
+  do_inf$var <- gsub("chl_mean", "chl-a", do_inf$var)
+  do_inf$var <- gsub("o2_mean_0m_ann", "DO, annual, 0m", do_inf$var)
+  do_inf$var <- gsub("o2_mean_250m", "DO, daily, 250m", do_inf$var)
+  do_inf$var <- gsub("ssh_mean", "SSH", do_inf$var)
+  do_inf$var <- gsub("mld_mean", "MLD", do_inf$var)
+  do_inf$var <- gsub("bathy_sd", "z_sd", do_inf$var)
+  
+  do_inf <- do_inf %>% mutate(model = "DO", iter = i) 
+  colnames(do_inf) <- c("var", "inf_val", "model", "iter")
+  inf_df <- rbind(inf_df, do_inf)
+  
+  }
+  
+  #agi model
+  for(i in 1:iter){
+  
+  agi_inf <- as.data.frame(ggBRT::ggInfluence(readRDS(agi_mod_files[i]), plot = FALSE)) %>% rownames_to_column()
+  colnames(agi_inf) <- c("var", "inf_val")
+  agi_inf$var <- gsub("\\<AGI_0m\\>", "AGI, daily, 0m", agi_inf$var)
+  agi_inf$var <- gsub("AGI_250m_ann", "AGI, annual, 250m", agi_inf$var)
+  agi_inf$var <- gsub("AGI_0m_seas", "AGI, seasonal, 0m", agi_inf$var)
+  agi_inf$var <- gsub("AGI_250m_seas", "AGI, seasonal, 250m", agi_inf$var)
+  agi_inf$var <- gsub("temp_mean", "temp", agi_inf$var)
+  agi_inf$var <- gsub("sal_mean", "sal", agi_inf$var)
+  agi_inf$var <- gsub("bathy_mean", "z", agi_inf$var)
+  agi_inf$var <- gsub("chl_mean", "chl-a", agi_inf$var)
+  agi_inf$var <- gsub("AGI_0m_ann", "AGI, annual, 0m", agi_inf$var)
+  agi_inf$var <- gsub("AGI_250m", "AGI, daily, 250m", agi_inf$var)
+  agi_inf$var <- gsub("ssh_mean", "SSH", agi_inf$var)
+  agi_inf$var <- gsub("mld_mean", "MLD", agi_inf$var)
+  agi_inf$var <- gsub("bathy_sd", "z_sd", agi_inf$var)
+  
+  agi_inf <- agi_inf %>% mutate(model = "AGI", iter = i) 
+  colnames(agi_inf) <- c("var", "inf_val", "model", "iter")
+  inf_df <- rbind(inf_df, agi_inf)
+  
+  }
+  
+  #combo model
+  for(i in 1:iter){
+  
+  do_agi_inf <- as.data.frame(ggBRT::ggInfluence(readRDS(combo_mod_files[i]), plot = FALSE)) %>% rownames_to_column()
+  colnames(do_agi_inf) <- c("var", "inf_val")
+  do_agi_inf$var <- gsub("\\<o2_mean_0m\\>", "DO, daily, 0m", do_agi_inf$var)
+  do_agi_inf$var <- gsub("AGI_250m_ann", "AGI, annual, 250m", do_agi_inf$var)
+  do_agi_inf$var <- gsub("o2_mean_0m_seas", "DO, seasonal, 0m", do_agi_inf$var)
+  do_agi_inf$var <- gsub("AGI_250m_seas", "AGI, seasonal, 250m", do_agi_inf$var)
+  do_agi_inf$var <- gsub("temp_mean", "temp", do_agi_inf$var)
+  do_agi_inf$var <- gsub("sal_mean", "sal", do_agi_inf$var)
+  do_agi_inf$var <- gsub("bathy_mean", "z", do_agi_inf$var)
+  do_agi_inf$var <- gsub("chl_mean", "chl-a", do_agi_inf$var)
+  do_agi_inf$var <- gsub("o2_mean_0m_ann", "DO, annual, 0m", do_agi_inf$var)
+  do_agi_inf$var <- gsub("AGI_250m", "AGI, daily, 250m", do_agi_inf$var)
+  do_agi_inf$var <- gsub("ssh_mean", "SSH", do_agi_inf$var)
+  do_agi_inf$var <- gsub("mld_mean", "MLD", do_agi_inf$var)
+  do_agi_inf$var <- gsub("bathy_sd", "z_sd", do_agi_inf$var)
+  
+  do_agi_inf <- do_agi_inf %>% mutate(model = "DO+AGI combo", iter = i) 
+  colnames(do_agi_inf) <- c("var", "inf_val", "model", "iter")
+  inf_df <- rbind(inf_df, do_agi_inf)
+  }
+  
+  return(inf_df)
+  
+} #end function
 
 
 
--- scripts/8b_performance_metric_loop.R
+++ scripts/8b_performance_metric_loop.R
@@ -7,36 +7,26 @@
  library(dismo)
   set.seed(1004)}
 
-### data ####
-#### CRW daily data ####
+### load data ####
+# CRW daily data 
 dat_base_d <- readRDS(here("data/locs_brts/crw_pas/dat_base.rds")) %>% mutate(tag = as.factor(tag))
 dat_do_d <- readRDS(here("data/locs_brts/crw_pas/dat_do.rds")) %>% mutate(tag = as.factor(tag))
 dat_agi_d <- readRDS(here("data/locs_brts/crw_pas/dat_agi.rds")) %>% mutate(tag = as.factor(tag))
 
-#### CRW seasonal data ####
+#### CRW seasonal data 
 dat_base_s <- readRDS(here("data/locs_brts/crw_pas_seas/dat_base_seas.rds")) %>% mutate(tag = as.factor(tag))
 dat_do_s <- readRDS(here("data/locs_brts/crw_pas_seas/dat_do_seas.rds")) %>% mutate(tag = as.factor(tag))
 dat_agi_s <- readRDS(here("data/locs_brts/crw_pas_seas/dat_agi_seas.rds")) %>% mutate(tag = as.factor(tag))
 
-#### CRW annual data ####
+#### CRW annual data 
 dat_base_a <- readRDS(here("data/locs_brts/crw_pas_ann/dat_base_ann.rds")) %>% mutate(tag = as.factor(tag))
 dat_do_a <- readRDS(here("data/locs_brts/crw_pas_ann/dat_do_ann.rds")) %>% mutate(tag = as.factor(tag))
 dat_agi_a <- readRDS(here("data/locs_brts/crw_pas_ann/dat_agi_ann.rds")) %>% mutate(tag = as.factor(tag))
 
-#### Add random variable for predictor selection ####
-pred_var <- rnorm(31084, mean = 50, sd = 10)
-
-dat_base_d$pred_var <- pred_var
-dat_do_d$pred_var <- pred_var
-dat_agi_d$pred_var <- pred_var
-
-dat_base_a$pred_var <- pred_var
-dat_base_s$pred_var <- pred_var
-
-#### Add seasonal and annual data to daily data df ####
+#### Add seasonal and annual data to daily data df 
 dat_do_all <- cbind(dat_do_d, dat_do_s$o2_mean_0m, dat_do_s$o2_mean_60m, dat_do_s$o2_mean_250m, dat_do_a$o2_mean_0m, dat_do_a$o2_mean_60m, dat_do_a$o2_mean_250m)
 dat_do_all <- dat_do_all %>%
-  rename("o2_mean_0m_seas" = "dat_do_s$o2_mean_0m", 
+  dplyr::rename("o2_mean_0m_seas" = "dat_do_s$o2_mean_0m", 
          "o2_mean_60m_seas" = "dat_do_s$o2_mean_60m", 
          "o2_mean_250m_seas" = "dat_do_s$o2_mean_250m", 
          "o2_mean_0m_ann" = "dat_do_a$o2_mean_0m", 
@@ -45,40 +35,23 @@ dat_do_all <- dat_do_all %>%
 
 dat_agi_all <- cbind(dat_agi_d, dat_agi_s$AGI_0m, dat_agi_s$AGI_60m, dat_agi_s$AGI_250m, dat_agi_a$AGI_0m, dat_agi_a$AGI_60m, dat_agi_a$AGI_250m)
 dat_agi_all <- dat_agi_all %>%
-  rename("AGI_0m_seas" = "dat_agi_s$AGI_0m",
+  dplyr::rename("AGI_0m_seas" = "dat_agi_s$AGI_0m",
          "AGI_60m_seas" = "dat_agi_s$AGI_60m", 
          "AGI_250m_seas" = "dat_agi_s$AGI_250m", 
          "AGI_0m_ann" = "dat_agi_a$AGI_0m", 
          "AGI_60m_ann" = "dat_agi_a$AGI_60m", 
          "AGI_250m_ann" = "dat_agi_a$AGI_250m")
 
-### combined DO and AGI df ####
-dat_do_agi_all <- cbind(dat_do_all, dat_agi_all[,c(19, 22, 24, 26, 27, 29)])
+### combined DO and AGI df 
+dat_do_agi_all <- cbind(dat_do_all, dat_agi_all[,c(22, 25, 28)])
 
-### split into test and train daily data #####
+### split data ####
+#### entire domain and study period ####
+# split into test and train daily data 75/25
 #base
 dat_base_temp <- floor((nrow(dat_base_d)/4)*3) #define % of training and test set
 dat_train_base_d <- dat_base_d[sample(nrow(dat_base_d),dat_base_temp),]
 dat_test_base_d <- dat_base_d[sample(nrow(dat_base_d),nrow(dat_base_d)-dat_base_temp),]
-
-#do
-dat_do_temp <- floor((nrow(dat_do_d)/4)*3) #define % of training and test set
-dat_train_do_d <- dat_do_d[sample(nrow(dat_do_d),dat_do_temp),]
-dat_test_do_d <- dat_do_d[sample(nrow(dat_do_d),nrow(dat_do_d)-dat_do_temp),]
-
-#agi
-dat_agi_temp <- floor((nrow(dat_agi_d)/4)*3) #define % of training and test set
-dat_train_agi_d <- dat_agi_d[sample(nrow(dat_agi_d),dat_agi_temp),]
-dat_test_agi_d <- dat_agi_d[sample(nrow(dat_agi_d),nrow(dat_agi_d)-dat_agi_temp),]
-
-### split into test and train seasonal/annual data####
-dat_base_temp_s <- floor((nrow(dat_base_s)/4)*3)
-dat_train_base_s <- dat_base_s[sample(nrow(dat_base_s),dat_base_temp_s),] #seasonal
-dat_test_base_s <- dat_base_s[sample(nrow(dat_base_s),nrow(dat_base_s)-dat_base_temp_s),]
-
-dat_base_temp_a <- floor((nrow(dat_base_a)/4)*3)
-dat_train_base_a <- dat_base_a[sample(nrow(dat_base_a),dat_base_temp_a),] #annual
-dat_test_base_a <- dat_base_a[sample(nrow(dat_base_a),nrow(dat_base_a)-dat_base_temp_a),]
 
 #do
 dat_do_temp_all <- floor((nrow(dat_do_all)/4)*3) #define % of training and test set
@@ -95,27 +68,63 @@ dat_do_agi_temp_all <- floor((nrow(dat_do_agi_all)/4)*3) #define % of training a
 dat_train_do_agi_all <- dat_do_agi_all[sample(nrow(dat_do_agi_all),dat_do_agi_temp_all),]
 dat_test_do_agi_all <- dat_do_agi_all[sample(nrow(dat_do_agi_all),nrow(dat_do_agi_all)-dat_do_agi_temp_all),]
 
+#### CCS (split CCS values based on Brodie et al., 2018) ####
+# split so train is domain - CCS, and test is CCS
+#base
+dat_train_base_d_ccs <- dat_base_d %>% filter(lat <= 30 | lat >= 48 | lon <= -134) 
+dat_test_base_d_ccs <- dat_base_d %>% filter(lat > 30 & lat < 48 & lon > -134) 
+
+#do 
+dat_train_do_all_ccs <- dat_do_all %>% filter(lat <= 30 | lat >= 48 | lon <= -134) 
+dat_test_do_all_ccs <- dat_do_all %>% filter(lat > 30 & lat < 48 & lon > -134)
+
+#agi 
+dat_train_agi_all_ccs <- dat_agi_all %>% filter(lat <= 30 | lat >= 48 | lon <= -134)
+dat_test_agi_all_ccs <- dat_agi_all %>% filter(lat > 30 & lat < 48 & lon > -134)
+
+#do + agi
+dat_train_do_agi_all_ccs <- dat_do_agi_all %>% filter(lat <= 30 | lat >= 48 | lon <= -134)
+dat_test_do_agi_all_ccs <- dat_do_agi_all %>% filter(lat > 30 & lat < 48 & lon > -134)
+
+#### NEC (split at 12 lat is northern boundary of NEC described in Byrne et al., 2024) ####
+# split so train is domain - NEC, and test is NEC
+#base
+dat_train_base_d_nec <- dat_base_d %>% filter(lat >= 12) 
+dat_test_base_d_nec <- dat_base_d %>% filter(lat < 12) 
+
+#do 
+dat_train_do_all_nec <- dat_do_all %>% filter(lat >= 12) 
+dat_test_do_all_nec <- dat_do_all %>% filter(lat < 12)
+
+#agi 
+dat_train_agi_all_nec <- dat_agi_all %>% filter(lat >= 12)
+dat_test_agi_all_nec <- dat_agi_all %>% filter(lat < 12)
+
+#do + agi
+dat_train_do_agi_all_nec <- dat_do_agi_all %>% filter(lat >= 12)
+dat_test_do_agi_all_nec <- dat_do_agi_all %>% filter(lat < 12)
+
 ### function to run for loop across cores to run model simulations ####
 n_cores <- detectCores()
 cluster <- makeCluster(n_cores-2)
 registerDoParallel(cluster)
 
-brt_run_sims <- function(train_dat, n_iter = 20, mod_type){
+brt_run_sims <- function(train_dat, n_iter = 20, mod_type, save_folder){
+  
   if(mod_type == "base"){
-    pred_vars = c(8:10, 15:18)
+    pred_vars = c("chl_mean", "temp_mean", "sal_mean", "ssh_mean", "mld_mean", "bathy_mean", "bathy_sd")
   }
   if(mod_type == "do"){
-    pred_vars = c(8:11, 16:19, 22, 24, 26, 27, 29)
+    pred_vars = c("chl_mean", "temp_mean", "sal_mean", "ssh_mean", "mld_mean", "bathy_mean", "bathy_sd", "o2_mean_0m", "o2_mean_250m", "o2_mean_0m_seas", "o2_mean_0m_ann", "o2_mean_250m_seas", "o2_mean_250m_ann")
   }
   if(mod_type == "agi"){
-    pred_vars = c(8:10, 15:19, 22, 24, 26, 27, 29)
+    pred_vars = c("chl_mean", "temp_mean", "sal_mean", "ssh_mean", "mld_mean", "bathy_mean", "bathy_sd", "AGI_0m", "AGI_250m", "AGI_0m_seas", "AGI_0m_ann", "AGI_250m_seas", "AGI_250m_ann")
   }
   if(mod_type == "combo"){
-    pred_vars = c(8:11, 16:19, 24, 27, 31,33, 35)
+    pred_vars = c("chl_mean", "temp_mean", "sal_mean", "ssh_mean", "mld_mean", "bathy_mean", "bathy_sd", "o2_mean_0m", "o2_mean_0m_seas", "o2_mean_0m_ann", "AGI_250m", "AGI_250m_seas", "AGI_250m_ann")
   }
   
   foreach(i = 1:n_iter, .packages = c("here", "gbm", "dismo", "tidyverse")) %dopar% {
-    
     try(brt_iter <- dismo::gbm.step(
       data = train_dat, 
       gbm.x = pred_vars, 
@@ -129,7 +138,7 @@ brt_run_sims <- function(train_dat, n_iter = 20, mod_type){
     )
     )
     
-  saveRDS(brt_iter, file = here(paste0("data/brt/mod_outputs/perf_metric_iters/", mod_type, "/", mod_type,"_", i, ".rds")))  
+  saveRDS(brt_iter, file = here(paste0(save_folder, mod_type, "/", mod_type,"_", i, ".rds")))  
   
   #end parallel
   }
@@ -138,23 +147,50 @@ brt_run_sims <- function(train_dat, n_iter = 20, mod_type){
 }
 
 ### run brt iterations ####
-#base model
-brt_run_sims(train_dat = dat_train_base_d, mod_type = "base")
+#### entire domain and study period 
+#### base model ####
+brt_run_sims(train_dat = dat_train_base_d, mod_type = "base", save_folder = "data/brt/mod_outputs/perf_metric_iters/")
 
 #do model
-brt_run_sims(train_dat = dat_train_do_all, mod_type = "do")
+brt_run_sims(train_dat = dat_train_do_all, mod_type = "do", save_folder = "data/brt/mod_outputs/perf_metric_iters/")
 
 #agi model
-brt_run_sims(train_dat = dat_train_agi_all, mod_type = "agi")
+brt_run_sims(train_dat = dat_train_agi_all, mod_type = "agi", save_folder = "data/brt/mod_outputs/perf_metric_iters/")
 
 #combo model
-brt_run_sims(train_dat = dat_train_do_agi_all, mod_type = "combo")
+brt_run_sims(train_dat = dat_train_do_agi_all, mod_type = "combo", save_folder = "data/brt/mod_outputs/perf_metric_iters/")
+
+#### CCS ####
+#base
+brt_run_sims(train_dat = dat_train_base_d_ccs, mod_type = "base", save_folder = "data/brt/mod_outputs/perf_metric_iters/CCS/")
+
+#do
+brt_run_sims(train_dat = dat_train_do_all_ccs, mod_type = "do", save_folder = "data/brt/mod_outputs/perf_metric_iters/CCS/")
+
+#agi
+brt_run_sims(train_dat = dat_train_agi_all_ccs, mod_type = "agi", save_folder = "data/brt/mod_outputs/perf_metric_iters/CCS/")
+
+#combo
+brt_run_sims(train_dat = dat_train_do_agi_all_ccs, mod_type = "combo", save_folder = "data/brt/mod_outputs/perf_metric_iters/CCS/")
+
+#### NEC ####
+#base
+brt_run_sims(train_dat = dat_train_base_d_nec, mod_type = "base", save_folder = "data/brt/mod_outputs/perf_metric_iters/NEC/")
+
+#do
+brt_run_sims(train_dat = dat_train_do_all_nec, mod_type = "do", save_folder = "data/brt/mod_outputs/perf_metric_iters/NEC/")
+
+#agi
+brt_run_sims(train_dat = dat_train_agi_all_nec, mod_type = "agi", save_folder = "data/brt/mod_outputs/perf_metric_iters/NEC/")
+
+#combo
+brt_run_sims(train_dat = dat_train_do_agi_all_nec, mod_type = "combo", save_folder = "data/brt/mod_outputs/perf_metric_iters/NEC/")
 
 stopCluster(cl = cluster)
 
 ### function to pull model perform metrics and store ####
-brt_perf_metric <- function(mod_files, mod_type){
-  
+brt_perf_metric <- function(mod_files, mod_type, domain = "all"){
+
   for(i in 1:length(mod_files)){
     
     #create df for performance metric values
@@ -167,18 +203,47 @@ brt_perf_metric <- function(mod_files, mod_type){
     #load model file
     mod_file <- readRDS(mod_files[i])
     
-    #load test data file
-    if(mod_type == "base"){
+    #load test data files
+    #full domain test data
+    if(mod_type == "base" & domain == "all"){
       test_file = readRDS(here("data/brt/mod_eval/base_test_daily.rds"))
     }
-    if(mod_type == "do"){
+    if(mod_type == "do" & domain == "all"){
       test_file = readRDS(here("data/brt/mod_eval/do_test_daily_seasonal_annual.rds"))
     }
-    if(mod_type == "agi"){
+    if(mod_type == "agi" & domain == "all"){
       test_file = readRDS(here("data/brt/mod_eval/agi_test_daily_seasonal_annual.rds"))
     }
-    if(mod_type == "combo"){
+    if(mod_type == "combo" & domain == "all"){
       test_file = readRDS(here("data/brt/mod_eval/agi_do_test_daily_seasonal_annual.rds"))
+    }
+    
+    #CCS test data
+    if(mod_type == "base" & domain == "CCS"){
+      test_file = dat_test_base_d_ccs
+    }
+    if(mod_type == "do"  & domain == "CCS"){
+      test_file = dat_test_do_all_ccs
+    }
+    if(mod_type == "agi"  & domain == "CCS"){
+      test_file = dat_test_agi_all_ccs
+    }
+    if(mod_type == "combo"  & domain == "CCS"){
+      test_file = dat_test_do_agi_all_ccs
+    }
+    
+    #NEC test data
+    if(mod_type == "base" & domain == "NEC"){
+      test_file = dat_test_base_d_nec
+    }
+    if(mod_type == "do"  & domain == "NEC"){
+      test_file = dat_test_do_all_nec
+    }
+    if(mod_type == "agi"  & domain == "NEC"){
+      test_file = dat_test_agi_all_nec
+    }
+    if(mod_type == "combo"  & domain == "NEC"){
+      test_file = dat_test_do_agi_all_nec
     }
   
    #calc performance metrics 
@@ -207,7 +272,17 @@ brt_perf_metric <- function(mod_files, mod_type){
   #end for loop
   }
   
+  if(domain == "all"){
   saveRDS(perform_df, here(paste0("data/brt/mod_outputs/perf_metric_iters/", mod_type, "_metrics.rds")))
+  }
+  
+  if(domain == "CCS"){
+    saveRDS(perform_df, here(paste0("data/brt/mod_outputs/perf_metric_iters/CCS/", mod_type, "_metrics.rds")))
+  }
+  
+  if(domain == "NEC"){
+    saveRDS(perform_df, here(paste0("data/brt/mod_outputs/perf_metric_iters/NEC/", mod_type, "_metrics.rds")))
+  }
   
   return(perform_df)
   
@@ -215,6 +290,7 @@ brt_perf_metric <- function(mod_files, mod_type){
 }
 
 ### run brt iterations ####
+#performance metrics entire domain and study period
 #base model
 brt_perf_metric(mod_type = "base", mod_files = list.files(here("data/brt/mod_outputs/perf_metric_iters/base"), full.names = TRUE))
 
@@ -225,8 +301,33 @@ brt_perf_metric(mod_type = "do", mod_files = list.files(here("data/brt/mod_outpu
 brt_perf_metric(mod_type = "agi", mod_files = list.files(here("data/brt/mod_outputs/perf_metric_iters/agi"), full.names = TRUE))
 
 #combo model
-brt_perf_metric(mod_type = "combo", mod_files = list.files(here("data/brt/mod_outputs/perf_metric_iters/combo"), full.names = TRUE))### run brt iterations ####
+brt_perf_metric(mod_type = "combo", mod_files = list.files(here("data/brt/mod_outputs/perf_metric_iters/combo"), full.names = TRUE))
 
+#performance metrics for CCS entire study period
+#base model
+brt_perf_metric(mod_type = "base", domain = "CCS", mod_files = list.files(here("data/brt/mod_outputs/perf_metric_iters/CCS/base"), full.names = TRUE))
+
+#do model
+brt_perf_metric(mod_type = "do", domain = "CCS", mod_files = list.files(here("data/brt/mod_outputs/perf_metric_iters/CCS/do"), full.names = TRUE))
+
+#agi model
+brt_perf_metric(mod_type = "agi", domain = "CCS", mod_files = list.files(here("data/brt/mod_outputs/perf_metric_iters/CCS/agi"), full.names = TRUE))
+
+#combo model
+brt_perf_metric(mod_type = "combo", domain = "CCS", mod_files = list.files(here("data/brt/mod_outputs/perf_metric_iters/CCS/combo"), full.names = TRUE))
+
+#performance metrics for NEC entire study period 
+#base model
+brt_perf_metric(mod_type = "base", domain = "NEC", mod_files = list.files(here("data/brt/mod_outputs/perf_metric_iters/NEC/base"), full.names = TRUE))
+
+#do model
+brt_perf_metric(mod_type = "do", domain = "NEC", mod_files = list.files(here("data/brt/mod_outputs/perf_metric_iters/NEC/do"), full.names = TRUE))
+
+#agi model
+brt_perf_metric(mod_type = "agi", domain = "NEC", mod_files = list.files(here("data/brt/mod_outputs/perf_metric_iters/NEC/agi"), full.names = TRUE))
+
+#combo model
+brt_perf_metric(mod_type = "combo", domain = "NEC", mod_files = list.files(here("data/brt/mod_outputs/perf_metric_iters/NEC/combo"), full.names = TRUE))
 
 
 
